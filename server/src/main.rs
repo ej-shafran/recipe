c4e43a27aadea481c232a8d16eb1f23e808cb41d
ej-shafran <ejshafran@hilma.tech>
2023-06-02 02:49:43 +0300

    Move things to lib.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,4 @@
-#[macro_use]
-extern crate rocket;
-
-#[get("/")]
-async fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[launch]
+#[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/api", routes![index])
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rocket::{http::Status, local::blocking::Client};
-
-    #[test]
-    fn hello_world() {
-        let client = Client::tracked(rocket()).expect("valid rocket instance");
-        let response = client.get(format!("/api{}", uri!(index))).dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string(), Some(String::from("Hello, world!")));
-    }
+    server::rocket()
 }
