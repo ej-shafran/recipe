d48f7769c40c7158d522336c1ec6f6f8af64bb58
ej-shafran <ejshafran@hilma.tech>
2023-06-24 15:53:14 +0300

    Add globals support for test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,18 @@
+/// <reference types="vitest" />
+
+
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({ hot: !process.env.VITEST })],
   server: {
     proxy: {
       "/api": "http://localhost:8000",
     },
   },
+  test: {
+    globals: true
+  }
 });
