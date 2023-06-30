267c09b41cda198d4b284ac2c383354a15e39057
ej-shafran <ejshafran@hilma.tech>
2023-06-30 04:25:08 +0300

    Add validation to NewComment

--- client/src/lib/components/NewComment.svelte
+++ client/src/lib/components/NewComment.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import { createMutation, useQueryClient } from "@tanstack/svelte-query";
   import axios from "axios";
+  import { z } from "zod";
 
   export let id: string;
 
+  const schema = z.string().min(10).max(255);
+
   let value = "";
+  let error: string | null = null;
 
   const queryClient = useQueryClient();
 
@@ -28,12 +32,23 @@
   });
 
   function handleSubmit() {
-    $mutation.mutate(value);
+    const validation = schema.safeParse(value);
+
+    if (!validation.success) {
+      error = validation.error.format()._errors.join(", ");
+    } else {
+      error = null;
+      $mutation.mutate(value);
+    }
   }
 </script>
 
 <form action="#" on:submit|preventDefault={handleSubmit}>
   <input bind:value />
+
+  {#if !!error}
+    <p>{error}</p>
+  {/if}
 
   <button type="submit">Submit</button>
 </form>
