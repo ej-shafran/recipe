cd47b77052750439cfea4c3e4a9d9e13ecf9dcbc
ej-shafran <ejshafran@hilma.tech>
2023-07-03 22:03:01 +0300

    Load .env files in Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "cypress";
+import { config as dotenv } from "dotenv";
+
+let env = {};
+dotenv({ processEnv: env, path: "./.env" });
+dotenv({ processEnv: env, path: "./.env.local" });
 
 export default defineConfig({
+  env,
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
