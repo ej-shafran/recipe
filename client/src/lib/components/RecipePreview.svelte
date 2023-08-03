6b10d3818d36eb805776cf75b38e36b36778780d
ej-shafran <ejshafran@hilma.tech>
2023-08-04 02:21:36 +0300

    Add Delete button to recipe previews

--- client/src/lib/components/RecipePreview.svelte
+++ client/src/lib/components/RecipePreview.svelte
@@ -1,17 +1,42 @@
 <script lang="ts">
   import { Link } from "svelte-navigator";
+  import axios from "axios";
+  import { createMutation, useQueryClient } from "@tanstack/svelte-query";
+
   import type { RecipePreviewDTO } from "../common/dto/RecipePreview.dto";
+  import { getUserId } from "../common/functions/auth.functions";
 
   export let recipePreview: RecipePreviewDTO;
+  const userId = getUserId();
+
+  const queryClient = useQueryClient();
+
+  const mutation = createMutation({
+    mutationFn: async () => {
+      await axios.delete(`/api/recipe/${recipePreview.id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["browse-recipe-previews"]);
+    },
+  });
 </script>
 
-<div>
-  <h3>
-    <Link to="/recipe/{recipePreview.id}">
-      {recipePreview.title}
-    </Link>
-  </h3>
+{#if $mutation.isLoading}
+  <!-- TODO -->
+  <div>Loading...</div>
+{:else}
+  <div>
+    <h3>
+      <Link to="/recipe/{recipePreview.id}">
+        {recipePreview.title}
+      </Link>
+    </h3>
 
-  <p>Posted By: <span>{recipePreview.poster.username}</span></p>
-  <p>Comments: <span>{recipePreview.commentCount}</span></p>
-</div>
+    <p>Posted By: <span>{recipePreview.poster.username}</span></p>
+    <p>Comments: <span>{recipePreview.commentCount}</span></p>
+
+    {#if recipePreview.poster.id === userId}
+      <button on:click={() => $mutation.mutate()}>Delete</button>
+    {/if}
+  </div>
+{/if}
