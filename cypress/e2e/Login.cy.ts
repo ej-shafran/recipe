e43f970ed9183e4fe8f83cb4cee5a76b6391dd4e
ej-shafran <ejshafran@hilma.tech>
2023-08-06 00:50:43 +0300

    Use `faker` for Login

--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 /// <reference path="../support/commands.ts" />
 
-import { TEST_USER, INVALID_USER } from "../support/constants";
+import { faker } from "@faker-js/faker";
 
 const FORM = "[data-cy=LOGIN_FORM]";
 
@@ -13,26 +13,43 @@ const SELECTORS = {
   HOME_HEADER: `[data-cy=HOME_HEADER]`,
 };
 
+const seed = faker.number.int();
+faker.seed(seed);
+
+const user = {
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+};
+
+faker.seed(seed + 1);
+const invalid = {
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+};
+
 const INVALID_CREDENTIALS = "Invalid credentials. Please try again.";
 
 describe("Login Page", () => {
+  before(() => {
+    // create the user
+    cy.request("POST", "/api/user/register", user);
+  });
+
   beforeEach(() => cy.visit("/login"));
 
   it("should display an error under the username when the credentials are wrong", () => {
-    cy.get(SELECTORS.USERNAME_INPUT).type(INVALID_USER.USERNAME);
-    cy.get(SELECTORS.PASSWORD_INPUT).type(INVALID_USER.PASSWORD);
+    cy.get(SELECTORS.USERNAME_INPUT).type(invalid.username);
+    cy.get(SELECTORS.PASSWORD_INPUT).type(invalid.password);
     cy.get(SELECTORS.SUBMIT_BUTTON).click();
 
     cy.get(SELECTORS.USERNAME_ERROR).should("contain", INVALID_CREDENTIALS);
   });
 
   it("should correctly log in when correct credentials are used", () => {
-    cy.get(SELECTORS.USERNAME_INPUT).type(TEST_USER.USERNAME);
-    cy.get(SELECTORS.PASSWORD_INPUT).type(TEST_USER.PASSWORD);
+    cy.get(SELECTORS.USERNAME_INPUT).type(user.username);
+    cy.get(SELECTORS.PASSWORD_INPUT).type(user.password);
     cy.get(SELECTORS.SUBMIT_BUTTON).click();
 
     cy.get(SELECTORS.HOME_HEADER).should("exist");
   });
 });
-
-export { };
