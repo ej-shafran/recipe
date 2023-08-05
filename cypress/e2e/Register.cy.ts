c2f2a6f34e9d917d883fdefa052fc10a67339db4
ej-shafran <ejshafran@hilma.tech>
2023-08-06 00:42:54 +0300

    Use property testing for Register

--- cypress/e2e/Register.cy.ts
+++ cypress/e2e/Register.cy.ts
@@ -1,7 +1,12 @@
 /// <reference types="cypress" />
 /// <reference path="../support/commands.ts" />
 
-import { TEST_USER } from "../support/constants";
+import * as fc from "fast-check";
+
+type User = {
+  username: string;
+  password: string;
+};
 
 const FORM = "[data-cy=REGISTER_FORM]";
 
@@ -14,42 +19,37 @@ const SELECTORS = {
   HOME_HEADER: `[data-cy=HOME_HEADER]`,
 };
 
-const NEW_USER = {
-  USERNAME: "NEW_USER",
-  PASSWORD: "abcdefgh",
-};
+function fillInForm(user: { username: string; password: string }) {
+  cy.get(SELECTORS.USERNAME_INPUT).type(user.username);
+  cy.get(SELECTORS.PASSWORD_INPUT).type(user.password);
+  cy.get(SELECTORS.CONFIRM_PASSWORD_INPUT).type(user.password);
+  cy.get(SELECTORS.SUBMIT_BUTTON).click();
+}
 
 const INVALID_CREDENTIALS = "Invalid credentials. Please try again.";
 
 describe("Register Page", () => {
-  before(() => cy.resetDB());
+  it("should properly create users in the database", () => {
+    const userArb = fc.record<User>({
+      username: fc.base64String({ minLength: 20, maxLength: 30 }),
+      password: fc.base64String({ minLength: 20 }),
+    });
 
-  beforeEach(() => cy.visit("/register"));
+    const prop = fc.property(userArb, (user) => {
+      fillInForm(user);
+      // we can't reset a request interception so we're forced to use milliseconds
+      cy.wait(1000);
+      cy.login(user.username, user.password).its("status").should("equal", 200);
+      cy.get(SELECTORS.HOME_HEADER).should("exist");
 
-  it("should display an error when an existing user is entered", () => {
-    cy.get(SELECTORS.USERNAME_INPUT).type(TEST_USER.USERNAME);
-    cy.get(SELECTORS.PASSWORD_INPUT).type(TEST_USER.PASSWORD);
-    cy.get(SELECTORS.CONFIRM_PASSWORD_INPUT).type(TEST_USER.PASSWORD);
-    cy.get(SELECTORS.SUBMIT_BUTTON).click();
+      cy.visit("/register");
+      fillInForm(user);
+      cy.get(SELECTORS.USERNAME_ERROR).should("contain", INVALID_CREDENTIALS);
+    });
 
-    cy.get(SELECTORS.USERNAME_ERROR).should("contain", INVALID_CREDENTIALS);
-  });
-
-  it("should successfully add a new user with new values", () => {
-    cy.intercept("POST", "/api/user/register").as("register");
-
-    cy.get(SELECTORS.USERNAME_INPUT).type(NEW_USER.USERNAME);
-    cy.get(SELECTORS.PASSWORD_INPUT).type(NEW_USER.PASSWORD);
-    cy.get(SELECTORS.CONFIRM_PASSWORD_INPUT).type(NEW_USER.PASSWORD);
-    cy.get(SELECTORS.SUBMIT_BUTTON).click();
-
-    cy.wait("@register");
-
-    cy.login(NEW_USER.USERNAME, NEW_USER.PASSWORD)
-      .its("status")
-      .should("equal", 200);
-    cy.get(SELECTORS.HOME_HEADER).should("exist");
+    fc.assert(
+      prop.beforeEach(() => cy.visit("/register")),
+      { numRuns: 5 }
+    );
   });
 });
-
-export { };
